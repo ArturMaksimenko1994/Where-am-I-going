c5db40a889db7a5cd992afaeb3f2e3e4365385d7
Artur <arturmaksimenko784@gmail.com>
2021-11-19 03:29:03 +0300

    ver 1.0

---
+++ js/index.js
@@ -0,0 +1,77 @@
+
+ let modalBtn = document.querySelectorAll(".modal-btn");
+ let closeBtn = document.getElementById("close-btn");
+ let modalShadow = document.getElementById("modal-shadow");
+ let modalPopup = document.getElementById("modal-popup");
+
+ for (var i = 0 ; i < modalBtn.length; i++) {
+     modalBtn[i].addEventListener('click', function () {
+         modalShadow.classList.remove("hidden");
+         modalPopup.classList.remove("hidden");
+         modalPopup.classList.add("popup-top");
+    });
+}
+
+ closeBtn.addEventListener("click", function () {
+     modalShadow.classList.add("hidden");
+     modalPopup.classList.remove("popup-top");
+ })
+
+ window.onclick = function (event) {
+     if (event.target === modalShadow) {
+         modalShadow.classList.add("hidden");
+         modalPopup.classList.remove("popup-top");
+     }
+ }
+//----------animation----------
+function onEntry(entry) {
+  entry.forEach(change => {
+      if (change.isIntersecting) {
+          change.target.classList.add('wish_animation_element');
+      }
+  });
+}
+let options = {
+  threshold: [0.5]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.wish');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
+//----------animationLeft----------
+function onEntryLeft(entryanim) {
+  entryanim.forEach(changeLeft => {
+      if (changeLeft.isIntersecting) {
+          changeLeft.target.classList.add('terrain_show_left');
+      }
+  });
+}
+let optionsLeft = {
+  threshold: [0.5]
+};
+let observerLeft = new IntersectionObserver(onEntryLeft, optionsLeft);
+let elementsLeft = document.querySelectorAll('.terrain_animation_left');
+
+for (let elmLeft of elementsLeft) {
+  observerLeft.observe(elmLeft);
+}
+//----------animationRight----------
+function onEntryRight(entryright) {
+  entryright.forEach(changeRight => {
+      if (changeRight.isIntersecting) {
+          changeRight.target.classList.add('terrain_show_right');
+      }
+  });
+}
+let optionsRight = {
+  threshold: [0.5]
+};
+let observerRight = new IntersectionObserver(onEntryRight, optionsRight);
+let elementsRight = document.querySelectorAll('.terrain_animation_right');
+
+for (let elmRight of elementsRight) {
+  observerRight.observe(elmRight);
+}
+
